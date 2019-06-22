c45973d6eb2c2486f95687fbbe8177fa18ae4c0b
Пылин Владимир <markizurasik@yandex.ru>
2019-06-23 01:36:49 +0500

    Добавлена проверка на совпадение сертификата и закрытого ключа

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# pycryptodome 3.4.3
 #
 #  routes.py
 #  
@@ -26,6 +27,11 @@ from flask import render_template
 from app import app
 from pathlib import Path
 from OpenSSL import crypto
+from Crypto.Util import asn1
+
+path_to_public_cert = '/home/marka/Загрузки/Folder/'
+path_to_private_key = '/home/marka/Загрузки/Folder/trusted.key'
+
 
 @app.route('/')
 @app.route('/index')
@@ -40,7 +46,7 @@ def all_certificates():
     return render_template('all_certificates.html', title='all_certificates', all_cert = all_cert)
 
 
-def search_for_certs(path_to_folder):
+def search_for_certificates(path_to_folder):
     """Поиск всех сертификатов в директории."""
     p = Path(path_to_folder).glob('*.crt')
     files = [x for x in p if x.is_file()]
@@ -54,13 +60,43 @@ def info_about_cert(certificate):
     issuer = cert.get_issuer().CN
     
     time_start = cert.get_notBefore()
-    time_start = str(time_start, 'utf-8')[:14]
-    time_start = datetime.strptime(time_start, "%Y%m%d%H%M%S")
+    time_start = str(time_start, 'utf-8')
+    time_start = datetime.strptime(time_start, "%Y%m%d%H%M%SZ")
     time_end = cert.get_notAfter()
-    time_end = str(time_end, 'utf-8')[:14]
-    time_end = datetime.strptime(time_end, "%Y%m%d%H%M%S")
+    time_end = str(time_end, 'utf-8')
+    time_end = datetime.strptime(time_end, "%Y%m%d%H%M%SZ")
     return time_start, time_end, subject, issuer
-            
+
+
+def check_certificate(doubtful_certificate, private_key):
+    '''Проверка совпадают ли сертификат с закрытым ключом'''
+    
+    # Загружаем сертификат и ключ
+    open_cert = open(str(doubtful_certificate)).read()
+    doubtful_certificate = crypto.load_certificate(crypto.FILETYPE_PEM, open_cert)
+    doubtful_certificate = doubtful_certificate.get_pubkey()    
+    open_key = open(str(private_key)).read()
+    private_key = Path(private_key)
+    private_key = crypto.load_privatekey(crypto.FILETYPE_PEM, open_key)
+    
+    pub_asn1=crypto.dump_privatekey(crypto.FILETYPE_ASN1, doubtful_certificate)
+    priv_asn1=crypto.dump_privatekey(crypto.FILETYPE_ASN1, private_key)
+    
+    # Расшифровываем DER
+    pub_der=asn1.DerSequence()
+    pub_der.decode(pub_asn1)
+    priv_der=asn1.DerSequence()
+    priv_der.decode(priv_asn1)
+    
+    # Получаем модуль
+    pub_modulus=pub_der[1]
+    priv_modulus=priv_der[1]
+     
+    if pub_modulus == priv_modulus:
+        return True
+    else:
+        return False
+        
     
 class Certificate():
     def __init__(self, time_start, time_end, subject, issuer):
@@ -70,9 +106,9 @@ class Certificate():
         else:
             self.time_start = time_start
         if type(time_end) is str:
-            self.time_end = datetime.strptime(time_start, "%d-%m-%Y-%H-%M")
+            self.time_end = datetime.strptime(time_end, "%d-%m-%Y-%H-%M")
         else:
-            self.time_end = time_start          
+            self.time_end = time_end          
         self.subject = subject
         self.issuer = issuer           
         
@@ -85,12 +121,10 @@ class Certificate():
             return False
 
 
-all_cert = []    
-all_cert.append(Certificate('01-01-1990-00-00', '01-01-2010-00-00', 'personal-site.com', 'CertCenter'))
-all_cert.append(Certificate('01-09-2019-00-00', '01-01-2020-00-00', 'big-company.com', 'CertCenter'))
-all_cert.append(Certificate('01-01-2018-00-00', '01-09-2019-00-00', 'my-site.com', 'CertCenter'))
+all_cert = []
 
-certs_in_directory = search_for_certs('/home/marka/Загрузки/Folder/')
+certs_in_directory = search_for_certificates(path_to_public_cert)
 for one_cert in certs_in_directory:
     time1, time2, who_to, who_from = info_about_cert(one_cert)
-    all_cert.append(Certificate(time1, time2, who_to, who_from))
+    if check_certificate(one_cert, path_to_private_key) == True:
+        all_cert.append(Certificate(time1, time2, who_to, who_from))
